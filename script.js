011a760e3cf88fdd3694f550d1d6a5082eea06a7
Claudiebl <claudie.labonte@hotmail.fr>
2025-12-16 09:08:15 +0100

    Esthétique et trajectoire terminé

--- script.js
+++ script.js
@@ -10,20 +10,4 @@ document.addEventListener("DOMContentLoaded", () => {
     home.classList.replace("fade-out", "fade-in");
   }, 1000);
 
-  // menus
-  menus.forEach((menu) => {
-    menu.addEventListener("click", (e) => {
-      const opened = menu.classList.contains("active");
-      menus.forEach((m) => m.classList.remove("active"));
-
-      if (!opened) menu.classList.add("active");
-    });
-  });
-
-  // fermeture AU CLIC EXTÉRIEUR → desktop uniquement
-  if (!isMobile) {
-    document.addEventListener("click", () => {
-      menus.forEach((menu) => menu.classList.remove("active"));
-    });
-  }
 });
